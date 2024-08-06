18cc75f1b31f2e201c163c847362f6935f6b8d5d
QuangLe1710 <160602994+QuangLe1710@users.noreply.github.com>
2024-08-06 10:36:15 +0700

    Again ?

---
+++ Buoi3String&List/lythuyet.py
@@ -0,0 +1,45 @@
+# s1 = "He nhi phan cua {0} duoc bieu dien la {0:b}".format(12) 
+
+# s2 = "Bieu dien duoi dang mu : {0:e}".format(1566.345)
+
+# s3 = "Mot phan ba : {0:.3f}".format(1/3)  
+
+# s4 = "|{:<10}|{:^10}|{:>10}|".format('123456789', 'tuple' , 'set')
+
+# s5 = "HIT PYTHON"
+
+# print(s1) 
+
+# print(s2)
+
+# print(s3)
+
+# print(s4)
+
+# for letter in s5 : 
+#     print(letter , end=" ")
+# print()
+
+# for i in range(len(s5)) : 
+#     print(s5[i]) 
+
+# print(s5[2] + s5[-1])
+
+# print(s5[0:3])
+
+# print(s5[0::3])
+
+# s = "H  I T P Y T H O N"
+# cnt = 0 
+# for i in s:
+#     if ( i != " " ) :
+#         cnt+= 1 
+#     else :
+#         continue  
+# print(f'Xau {s} co so tu la : {cnt} ')
+
+# print(len(input().split()))
+
+l = [x**2 for x in range(10)]
+
+print(l) 
